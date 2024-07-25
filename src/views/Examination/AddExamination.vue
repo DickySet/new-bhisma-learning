c41e9f00ccc7f69e33c7cb3d5d77226f9224b66b
Dickysst <dicset234@gmail.com>
2024-07-25 18:17:19 +0700

    ver 8

--- src/views/Examination/AddExamination.vue
+++ src/views/Examination/AddExamination.vue
@@ -266,8 +266,9 @@
                                                 </svg>
                                             </div>
                                             
-                                            <div style="background-image: linear-gradient(to bottom, #489DEC 0%, #00387F 100%); padding: 4px 0px 0px 10px">
-                                                <label class="text-white">Pernyataan</label>
+                                            <div class="d-flex" style="gap: 10px; padding: 6px 0px 0px 0px">
+                                                <div style="background-image: linear-gradient(to bottom, #489DEC 0%, #00387F 100%); width: 20px; height: 20px; border-radius: 50%; "></div>
+                                                <h5 style="margin-top: 2px" class="font-weight-bold">Pernyataan</h5>
                                             </div>
 
                                             <textarea rows="4" placeholder="Masukan Pernyataan"></textarea>
@@ -275,8 +276,9 @@
                                             <!-- SOAL PILIHAN GANDA -->
                                                 <template v-if="chooseVersion == 1">
                                                 
-                                                    <div style="background-image: linear-gradient(to bottom, #489DEC 0%, #00387F 100%); padding: 4px 0px 0px 10px">
-                                                        <label class="text-white">Pertanyaan</label>
+                                                    <div class="d-flex" style="gap: 10px; padding: 6px 0px 0px 0px">
+                                                        <div style="background-image: linear-gradient(to bottom, #489DEC 0%, #00387F 100%); width: 20px; height: 20px; border-radius: 50%; "></div>
+                                                        <h5 style="margin-top: 2px" class="font-weight-bold">Pertanyaan</h5>
                                                     </div>
 
                                                     <textarea rows="4" placeholder="Masukan Soal"></textarea>
@@ -286,8 +288,9 @@
                                                         </div>
                                                         <div style="width: 92%">
                                                             <div class="d-flex justify-content-between" style="gap: 20px;">
-                                                                <div style="background-image: linear-gradient(to bottom, #489DEC 0%, #00387F 100%); padding: 4px 0px 0px 10px; width: 100%">
-                                                                    <label class="text-white">Tambah Jawaban</label>
+                                                                <div class="d-flex" style="gap: 10px; padding: 6px 0px 0px 0px">
+                                                                    <div style="background-image: linear-gradient(to bottom, #489DEC 0%, #00387F 100%); width: 20px; height: 20px; border-radius: 50%; "></div>
+                                                                    <h5 style="margin-top: 2px" class="font-weight-bold">Tambah Jawaban</h5>
                                                                 </div>
                                                                 <svg xmlns="http://www.w3.org/2000/svg" style="color: #00387F; margin-top: 8px" width="18" height="18" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
                                                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
@@ -299,20 +302,16 @@
                                                     
                                                     <template v-for="options in option">
                                                         <div class="d-flex">
-                                                            <div style="width: 8%;margin-top: 8px">
+                                                            <div style="width: 8%;">
                                                                 <h5>{{options.option_name}}.</h5>
                                                             </div>
                                                             <div style="width: 92%">
-                                                                <div class="d-flex justify-content-between" style="gap: 20px;">
-                                                                    <div style="background-image: linear-gradient(to bottom, #489DEC 0%, #00387F 100%); padding: 4px 0px 0px 10px; width: 100%">
-                                                                        <label class="text-white">Jawaban</label>
-                                                                    </div>
-                                                                    <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" style="color: red; margin-top: 8px" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                                                <div class="d-flex justify-content-between mb-3" style="gap: 20px;">
+                                                                    <!-- <h5 style="">Jawaban</h5> -->
+                                                                    <textarea rows="2" placeholder="Masukan Jawaban"></textarea>
+                                                                    <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" style="color: red;" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                                                                         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
                                                                     </svg>
-                                                                </div>
-                                                                <div class="mt-2">
-                                                                    <textarea rows="4" placeholder="Masukan Jawaban"></textarea>
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -325,8 +324,9 @@
                                             <!-- SOAL ESSAY -->
                                                 <template v-if="chooseVersion == 2">
                                                 
-                                                    <div style="background-image: linear-gradient(to bottom, #489DEC 0%, #00387F 100%); padding: 4px 0px 0px 10px">
-                                                        <label class="text-white">Pertanyaan</label>
+                                                    <div class="d-flex" style="gap: 10px; padding: 6px 0px 0px 0px">
+                                                        <div style="background-image: linear-gradient(to bottom, #489DEC 0%, #00387F 100%); width: 20px; height: 20px; border-radius: 50%; "></div>
+                                                        <h5 style="margin-top: 2px" class="font-weight-bold">Pertanyaan</h5>
                                                     </div>
 
                                                     <textarea rows="4" placeholder="Masukan Soal"></textarea>
@@ -337,8 +337,9 @@
                                             <!-- SOAL PILIHAN GANDA KOMPLEKS -->
                                                 <template v-if="chooseVersion == 3">
                                                 
-                                                    <div style="background-image: linear-gradient(to bottom, #489DEC 0%, #00387F 100%); padding: 4px 0px 0px 10px">
-                                                        <label class="text-white">Pertanyaan</label>
+                                                    <div class="d-flex" style="gap: 10px; padding: 6px 0px 0px 0px">
+                                                        <div style="background-image: linear-gradient(to bottom, #489DEC 0%, #00387F 100%); width: 20px; height: 20px; border-radius: 50%; "></div>
+                                                        <h5 style="margin-top: 2px" class="font-weight-bold">Pertanyaan</h5>
                                                     </div>
 
                                                     <textarea rows="4" placeholder="Masukan Soal"></textarea>
@@ -348,8 +349,9 @@
                                                         </div>
                                                         <div style="width: 92%">
                                                             <div class="d-flex justify-content-between" style="gap: 20px;">
-                                                                <div style="background-image: linear-gradient(to bottom, #489DEC 0%, #00387F 100%); padding: 4px 0px 0px 10px; width: 100%">
-                                                                    <label class="text-white">Tambah Jawaban</label>
+                                                                <div class="d-flex" style="gap: 10px; padding: 6px 0px 0px 0px">
+                                                                    <div style="background-image: linear-gradient(to bottom, #489DEC 0%, #00387F 100%); width: 20px; height: 20px; border-radius: 50%; "></div>
+                                                                    <h5 style="margin-top: 2px" class="font-weight-bold">Tambah Jawaban</h5>
                                                                 </div>
                                                                 <svg xmlns="http://www.w3.org/2000/svg" style="color: #00387F; margin-top: 8px" width="18" height="18" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
                                                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
@@ -365,16 +367,11 @@
                                                                 <h5>{{options.option_name}}.</h5>
                                                             </div>
                                                             <div style="width: 92%">
-                                                                <div class="d-flex justify-content-between" style="gap: 20px;">
-                                                                    <div style="background-image: linear-gradient(to bottom, #489DEC 0%, #00387F 100%); padding: 4px 0px 0px 10px; width: 100%">
-                                                                        <label class="text-white">Jawaban</label>
-                                                                    </div>
+                                                                <div class="d-flex justify-content-between mb-3" style="gap: 20px;">
+                                                                    <textarea rows="2" placeholder="Masukan Jawaban"></textarea>
                                                                     <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" style="color: red; margin-top: 8px" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                                                                         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
                                                                     </svg>
-                                                                </div>
-                                                                <div class="mt-2">
-                                                                    <textarea rows="4" placeholder="Masukan Jawaban"></textarea>
                                                                 </div>
                                                             </div>
                                                         </div>
